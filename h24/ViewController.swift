0e10aff594a84c3e8dd7cf0e6f0f818f016cc870
Kazimir Makaranka <m2014ky@gmail.com>
2023-04-06 10:43:17 +0300

    Initial Commit

---
+++ h24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  h24
+//
+//  Created by Kazimir on 6.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
